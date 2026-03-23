7cee00d449014cc957dc1e5daaf3e87f9f898be8
Imad Ahmed <j166a775433@gmail.com>
2026-03-23 11:11:43 +0000

    Initial commit

---
+++ TDDProject/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDProject
+{
+    public enum Point
+    {
+        NORTH,
+        EAST,
+        SOUTH,
+        WEST
+    }
+    public enum Direction
+    {
+        LEFT,
+        RIGHT
+    }
+}
